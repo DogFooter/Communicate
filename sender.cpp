ac440814d9c704e91ab826e8affc428a12788694
DogFooter <tmflffj4663@gmail.com>
2023-09-01 21:36:59 +0900

    CNamedPipeIPC 클래스 생성

--- sender.cpp
+++ sender.cpp
@@ -1,49 +1,118 @@
 #include <windows.h>
-
+#include <string>
 #include <iostream>
 
-#include "data.h"
-int main() {
-    InitialData initData = { 1, 2, 3, "Hello" };
-    // TODO: Pass initData to b.exe
-    HANDLE hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\MyPipe"), PIPE_ACCESS_DUPLEX,
-        PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
-        1, sizeof(ProgressData), sizeof(ResultData),
-        NMPWAIT_USE_DEFAULT_WAIT, NULL);
-    if (hPipe == INVALID_HANDLE_VALUE) {
-        std::cerr << "CreateNamedPipe failed" << std::endl;
-        return -1;
-    }
-    DWORD bytesWritten;
+#include "commData.h"
+class CNamedPipeIPC {
 
-    if (!ConnectNamedPipe(hPipe, NULL)) {
-        CloseHandle(hPipe);
-        std::cerr << "ConnectNamedPipe failed" << std::endl;
-        return -1;
+
+    HANDLE hPipe = INVALID_HANDLE_VALUE;
+
+public:
+
+    int CreateNamedPipeDefault(std::string_view s = "\\\\.\\pipe\\MyPipe") {
+
+        hPipe = CreateNamedPipe(std::wstring(s.begin(), s.end()).data(), PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
+            1, sizeof(ProgressData), sizeof(ResultData),
+            NMPWAIT_USE_DEFAULT_WAIT, NULL);
+
+
+        if (hPipe == INVALID_HANDLE_VALUE) {
+            std::cerr << "CreateNamedPipe failed" << std::endl;
+            return -1;
+        }
+        return 0;
+
     }
-    if (!WriteFile(hPipe, &initData, sizeof(initData), &bytesWritten, NULL)) {
-        CloseHandle(hPipe);
-        return -1;
+
+    int Connection() {
+
+        if (!ConnectNamedPipe(hPipe, NULL)) {
+            std::cerr << "ConnectNamedPipe failed" << std::endl;
+            return -1;
+        }
+        return 0;
     }
-    std::cout << "Waiting for client" << std::endl;
-    while (true) {
-        ProgressData progData;
+
+    DWORD WriteToPipe(ProgressData& data) {
+        DWORD bytesWritten;
+        if (!WriteFile(hPipe, &data, sizeof(data), &bytesWritten, NULL)) {
+            return 0;
+        }
+        return bytesWritten;
+    }
+
+    DWORD WriteToPipe(InitialData& data) {
+        DWORD bytesWritten;
+        if (!WriteFile(hPipe, &data, sizeof(data), &bytesWritten, NULL)) {
+            return 0;
+        }
+        return bytesWritten;
+    }
+
+    DWORD WriteToPipe(ResultData& data) {
+        DWORD bytesWritten;
+        if (!WriteFile(hPipe, &data, sizeof(data), &bytesWritten, NULL)) {
+            return 0;
+        }
+
+        std::cout << " Finish" << std::endl;
+        return bytesWritten;
+    }
+
+    DWORD ReadToPipe(ProgressData& data) {
         DWORD bytesRead;
-        if (!ReadFile(hPipe, &progData, sizeof(progData), &bytesRead, NULL)) break;
+        if (!ReadFile(hPipe, &data, sizeof(data), &bytesRead, NULL))
+            return 0;
 
-        progData.print();
-        // TODO: Do something with progDat  
-        // If you want to send data back to the client at this point:
-        // WriteFile(hPip&datatosendsizeof(datatosend)&bytesWritteNULL);  
+        data.print();
+        return bytesRead;
+
     }
-    ResultData resData;
-    DWORD bytesRead;
-    if (!ReadFile(hPipe, &resData, sizeof(resData), &bytesRead, NULL)) {
+
+    DWORD ReadToPipe(InitialData& data) {
+        DWORD bytesRead;
+        if (!ReadFile(hPipe, &data, sizeof(data), &bytesRead, NULL))
+            return 0;
+
+        return bytesRead;
+
+    }
+
+    DWORD ReadToPipe(ResultData& data) {
+        DWORD bytesRead;
+        if (!ReadFile(hPipe, &data, sizeof(data), &bytesRead, NULL))
+            return 0;
+
+        std::cout << "Finished" << std::endl;
+        return bytesRead;
+
+    }
+    ~CNamedPipeIPC() {
         CloseHandle(hPipe);
-        return -1;
+        std::cout << "Named Pipe cloesd" << std::endl;
     }
-    // TODO: Do something with resDat
-    CloseHandle(hPipe);
-    return(0);
+};
+
+int main() {
+
+    InitialData initData = { 1, 2, 3, "Hello" };
+    CNamedPipeIPC Sender;
+
+    Sender.CreateNamedPipeDefault();
+    Sender.Connection();;
+
+    std::cout << "Waiting for client" << std::endl;
+
+    ProgressData data;
+    while (!Sender.ReadToPipe(data));
+
+    ResultData resData;
+    if (!Sender.ReadToPipe(resData)) {
+        std::cout << "Receive Faild" << std::endl;
+    }
+
+    return 0;
 
 }
